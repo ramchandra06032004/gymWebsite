841faa5467d70b112bcebf8fe90486c41b3528d9
Ramchandra Mulik <rajumulik51@gmail.com>
2024-02-17 23:09:48 +0530

    day 4 code added

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-window.addEventListener("scroll", function () {
-  const elements = document.querySelectorAll(".animate-on-scroll");
-  const windowHeight = window.innerHeight;
-
-  elements.forEach(function (element) {
-    const elementPosition = element.getBoundingClientRect().top;
-    const elementHeight = element.getBoundingClientRect().height;
-    const animationStart = windowHeight - elementPosition;
-
-    if (animationStart > 0 && animationStart < windowHeight + elementHeight) {
-      element.classList.add("show");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
     } else {
-      element.classList.remove("show");
+      entry.target.classList.remove("show");
     }
   });
+});
+const hiddenElements = document.querySelectorAll(".hiddenLeft");
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observerRight = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenRightElements = document.querySelectorAll(".hiddenRight");
+hiddenRightElements.forEach((el) => observerRight.observe(el));
+
+document.querySelector(".closeBtn").addEventListener("click", function () {
+  document.querySelector(".menuContainer").style.display = "none";
+});
+
+document.querySelector(".menu").addEventListener("click", function () {
+  document.querySelector(".menuContainer").style.display = "block";
 });
